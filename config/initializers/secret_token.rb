4bc5b00bad4825d734cac1fe017513d6e1dc9def
ryan007 <goodboyryan007@gmail.com>
2012-08-17 12:08:08 +0800

    xmams

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmams::Application.config.secret_token = '625e98953e49a5e31789fc80e3cf0756f5c651fbed54f2ec57336bc89c93a9fc038da7c6de2973baaccf26ba12bb2204af14a7b91eae62a6cc8139e82b747a91'
