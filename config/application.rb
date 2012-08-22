a41f5278f2f22c225e011e087a5644735727d73b
ryan007 <goodboyryan007@gmail.com>
2012-08-22 18:21:42 +0800

    test

--- config/application.rb
+++ config/application.rb
@@ -31,10 +31,9 @@ module Xmams
     config.time_zone = 'Beijing'
     config.active_record.default_timezone = :local
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :cn
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
